3aa801b8b7b799ec6d5d57da3cf371d98564c969
Murteza12 <murteza.raja444@gmail.com>
2020-10-13 15:08:08 +0500

    Initial Commit

---
+++ techbayTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  techbayTask
+//
+//  Created by Murteza on 13/10/2020.
+//  Copyright © 2020 Murteza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
